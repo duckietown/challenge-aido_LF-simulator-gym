f6fd0f25e1b5dd027d2da254a885a4c5dcb00de0
Andrea Censi <acensi@ethz.ch>
2020-10-29 18:11:06 +0100

    Bump version: 6.0.9 → 6.0.10

--- gym_bridge.py
+++ gym_bridge.py
@@ -40,7 +40,7 @@ from gym_duckietown.simulator import (NotInLane, ObjMesh, ROBOT_LENGTH, ROBOT_WI
                                       WHEEL_DIST)
 
 logger = ZLogger('gym_bridge')
-__version__ = '6.0.9'
+__version__ = '6.0.10'
 
 logger.info(f'gym_bridge {__version__}')
 
