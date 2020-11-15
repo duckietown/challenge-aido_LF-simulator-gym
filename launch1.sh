de3e862847c48170b0a6b17833db265c74b8b136
Andrea Censi <acensi@ethz.ch>
2020-11-15 19:20:06 +0100

    mionr

---
+++ launch1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+prefix="$0 > "
+_kill_procs() {
+   echo "$prefix _kill_procs()"
+#  kill -TERM $gym
+#  wait $gym
+  kill -TERM $xvfb
+}
+
+
+## Setup a trap to catch SIGTERM and relay it to child processes
+trap _kill_procs SIGTERM
+trap _kill_procs INT
+
+# make sure there is no xserver running (only neded for docker-compose)
+#killall Xvfb || true
+rm -f /tmp/.X99-lock || true
+
+# Start Xvfbs
+echo "$prefix Starting xvfb"
+Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &
+xvfb=$!
+echo "$prefix Started xvfb with PID $xvfb" 1>&2
+export DISPLAY=:99
+
+
+echo "$prefix Now running : $1" 1>&2
+nice -n 20 $1
+ret=$?
+echo "$prefix gym_simulation_launcher terminated with return code $ret" 1>&2
+
+
+echo "$prefix Killing xvfb..." 1>&2
+kill $xvfb
+
+echo "$prefix Killed." 1>&2
+
+echo "$prefix Graceful exit of launch.sh with return code $ret" 1>&2
+exit $ret
