e6cc2875ba4744df09e5dfd9aca13e659684c164
Andrea Censi <github@censi.org>
2020-12-02 23:22:19 +0100

    fix

--- src/duckietown_simulator_gym/code.py
+++ src/duckietown_simulator_gym/code.py
@@ -251,7 +251,7 @@ class PC(R):
             if math.fabs(current_time - ti) > 5:
                 self.render_observations[i] = None
 
-        to_average = self.render_observations[-1]
+        to_average = [self.render_observations[-1]]
         if not to_average:
             msg = "Cannot find observations to average"
             logger.warning(
