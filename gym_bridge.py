037b1df572086fc73df05e9aadb8f99d5c7af55e
Andrea Censi <acensi@ethz.ch>
2021-06-18 18:26:05 +0200

    fix

--- gym_bridge.py
+++ gym_bridge.py
@@ -5,7 +5,7 @@
 import logging
 import os
 
-from aido_schemas import protocol_simulator_DB20, wrap_direct
+from aido_schemas import protocol_simulator_DB20_timestamps, wrap_direct
 from duckietown_simulator_gym.code import GymDuckiebotSimulator
 
 
@@ -30,7 +30,7 @@ def main():
     set_loglevel()
     # setup_logging()
     node = GymDuckiebotSimulator()
-    protocol = protocol_simulator_DB20
+    protocol = protocol_simulator_DB20_timestamps
     wrap_direct(node=node, protocol=protocol)
 
 
